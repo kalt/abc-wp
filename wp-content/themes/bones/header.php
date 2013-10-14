e086346da62dc9704215411cd9133b1e7a17c4ce
Pierre-Emmanuel Fringant <pierre.fringant@gmail.com>
2013-10-14 08:39:32 +0200

    Basic styling

--- wp-content/themes/bones/header.php
+++ wp-content/themes/bones/header.php
@@ -47,16 +47,17 @@
 
 				<div id="inner-header" class="wrap clearfix">
 
-					<!-- to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> -->
-					<p id="logo" class="h1"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
+					<div id="logo-wrapper">
+						<a id="logo-link" href="<?php echo home_url(); ?>" rel="nofollow" title="Retour à l'accueil">
+							<img id="logo-img" src="<?php echo get_template_directory_uri(); ?>/library/images/logo-abc.png" alt="<?php bloginfo('name'); ?>">
+						</a>
+					</div>
 
-					<!-- if you'd like to use the site description you can un-comment it below -->
 					<?php // bloginfo('description'); ?>
 
-
-					<nav role="navigation">
+					<!--<nav role="navigation">
 						<?php bones_main_nav(); ?>
-					</nav>
+					</nav>-->
 
 				</div> <!-- end #inner-header -->
 
