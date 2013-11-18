b815f9601c037a6f70384acfdb7b3b0f97014340
Pierre-Emmanuel Fringant <pierre.fringant@gmail.com>
2013-11-18 13:19:15 +0100

    Add Google Analytics

--- wp-content/themes/bones/header.php
+++ wp-content/themes/bones/header.php
@@ -34,8 +34,16 @@
 		<?php wp_head(); ?>
 		<!-- end of wordpress head -->
 
-		<!-- drop Google Analytics Here -->
-		<!-- end analytics -->
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-45830564-1']);
+		  _gaq.push(['_trackPageview']);
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	</head>
 
 	<body <?php body_class(); ?>>
